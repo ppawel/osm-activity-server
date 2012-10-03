3c793d8ee6428bd8ce20273ce1cf7550d90252a5
Paweł Paprota <ppawel@fastmail.fm>
2012-10-03 15:28:37 +0200

    Added Rails app.

---
+++ rails/app/models/activity.rb
@@ -0,0 +1,4 @@
+class Activity < ActiveRecord::Base
+  attr_accessor :content
+  attr_accessor :title
+end
---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivityServer::Application.config.secret_token = 'c20679e25d8ae5c381d4150c0306ade0fa7fce1a2162b1a9116d0e1ecee3226b4e5f88bbf27193e188afcab53b7cc7577401c7b1435c8413fc18d741df6bb9ac'
